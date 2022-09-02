e23da04d7d05d6496abf726596e145dba472f0f3
Dzybystim <103567184+Dzybystim@users.noreply.github.com>
2022-09-02 23:33:19 +0300

    add second exc

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
 // Change code below this line
 
 console.log(galleryItems);
+
+const containerGalleryElement = document.querySelector('.gallery');
+
+
+const arrayA = galleryItems.map((item) => {
+    
+    const aElement = document.createElement("a")
+    aElement.href = item.original;
+    aElement.classList.add("gallery__item");
+
+
+   const imgElement = document.createElement("img")
+   imgElement.classList.add("gallery__image")
+   imgElement.src = item.preview;
+   imgElement.alt = item.description;
+
+   aElement.append(imgElement);
+
+   return aElement;
+   
+})
+
+containerGalleryElement.append(...arrayA);
+
+let lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay: 250});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player'
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY_TIME_VIDEO = "videoplayer-current-time";
+
+
+const timeVideo = function(data) {
+    let secondsJSON = JSON.stringify(data.seconds);
+
+    localStorage.setItem(KEY_TIME_VIDEO, secondsJSON), 1000
+
+}
+
+let secondsTime = localStorage.getItem(KEY_TIME_VIDEO)
+
+player.setCurrentTime(secondsTime)
+
+
+player.on('timeupdate', throttle(timeVideo, 1000));
 
